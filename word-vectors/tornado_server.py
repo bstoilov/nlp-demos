d54c77f0fe98c766cc23080c27beba46e82bbbb8
bstoilov <boriostoilov@gmail.com>
2020-02-20 20:52:50 +0200

    clean up

--- word-vectors/tornado_server.py
+++ word-vectors/tornado_server.py
@@ -12,30 +12,30 @@ wv_es = '../models/wv_es/wv_es'
 wv_bg = '../models/wv_bg/wv_bg'
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+word_pairs = [
+    ("едно", "one"), ("две", "two"), ("три", "three"), ("четири", "four"), ("пет", "five"),
+    ("шест", "six"), ("седем", "seven"),
+    ("куче", "dog"), ("риба", "fish"), ("птици", "birds"),
+    ("ябълка", "apple"), ("портокал", "orange"), ("грозде", "grape"), ("банан", "banana")
+]
+
 
 class WordVectorProcessor:
 
     def __init__(self):
         self.word_vectors_en = api.load("glove-wiki-gigaword-100")
         self.word_vectors_bg = KeyedVectors.load(wv_bg, mmap='r')
-        word_pairs = [
-            ("едно", "one"), ("две", "two"), ("три", "three"), ("четири", "four"), ("пет", "five"),
-            ("шест", "six"), ("седем", "seven"),
-            ("куче", "dog"), ("риба", "fish"), ("птици", "birds"),
-            ("ябълка", "apple"), ("портокал", "orange"), ("грозде", "grape"), ("банан", "banana")
-        ]
-        self.translation_mat = TranslationMatrix(self.word_vectors_bg.wv, self.word_vectors_en.wv, word_pairs=word_pairs)
+        self.translation_mat = TranslationMatrix(self.word_vectors_bg.wv, self.word_vectors_en.wv,
+                                                 word_pairs=word_pairs)
         self.word_vectors_bg.wv.most_similar(positive=['крал'], negative=[], topn=vector_top_n)
-        self.word_vectors_en.wv.most_similar(positive=['amigo'], negative=[], topn=vector_top_n)
+        self.word_vectors_en.wv.most_similar(positive=['man'], negative=[], topn=vector_top_n)
         logging.info('Word vectors were loaded and initialized')
 
     def wv_similar(self, body):
         lang = body['lang']
         pos = body['positive']
         neg = body['negative']
-        vectors = self.word_vectors_bg
-        if lang is "es":
-            vectors = self.word_vectors_es
+        vectors = self.get_vector_model(lang)
         try:
             result = vectors.wv.most_similar(positive=pos, negative=neg, topn=vector_top_n)
         except Exception as e:
@@ -55,8 +55,8 @@ class WordVectorProcessor:
         return transl
 
     def get_vector_model(self, lang):
-        if lang == "es":
-            return self.word_vectors_es
+        if lang == "en":
+            return self.word_vectors_en
         return self.word_vectors_bg
 
 
@@ -71,19 +71,27 @@ class WvHandler(web.RequestHandler):
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
 
+    def options(self):
+        self.set_status(204)
+        self.finish()
+
     def post(self):
         data = escape.json_decode(self.request.body)
         response = word_vector_processor.wv_similar(data)
         logging.info('words resp ' + str(response))
         self.write(response)
 
+
+class TranslateHandler(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+
     def options(self):
-        # no body
         self.set_status(204)
         self.finish()
 
-
-class TranslateHandler(web.RequestHandler):
     def post(self):
         data = escape.json_decode(self.request.body)
         response = word_vector_processor.translate(data)
